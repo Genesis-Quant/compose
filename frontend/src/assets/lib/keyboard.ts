231f8f88a6b98e519422cdbb15a2e5d45aa05dd0
realhuhu <2661467107@qq.com>
2026-08-07 23:41:50 +0800

    chore: convert frontend into submodule

--- frontend/src/assets/lib/keyboard.ts
+++
@@ -1,5 +0,0 @@
-import type { KeyboardEvent } from "react";
-
-export function isInputMethodComposing(event: KeyboardEvent<HTMLElement>) {
-  return event.nativeEvent.isComposing || event.nativeEvent.keyCode === 229;
-}
--- frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "@/App";
-import "@/assets/styles/tailwind.css";
-import "@/assets/styles/base.less";
-import "@/assets/styles/transition.less";
-
-createRoot(document.getElementById("root")!).render(<BrowserRouter><App /></BrowserRouter>);
